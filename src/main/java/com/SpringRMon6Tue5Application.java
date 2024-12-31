67014dc597b33851b9bbd97bae1c5a5d11266443
Tejas Shah <tejasshah2k19@gmail.com>
2024-12-31 17:40:26 +0530

    first

---
+++ src/main/java/com/SpringRMon6Tue5Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRMon6Tue5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRMon6Tue5Application.class, args);
+	}
+
+}
